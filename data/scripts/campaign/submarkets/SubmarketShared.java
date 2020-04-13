0c98857900d8f1e2a7c690c647d4e5a4d93bf8ca
Brian Deale <bhdeale@gmail.com>
2020-04-13 14:19:20 -0400

    open market ships working both ways and extra messaging

--- data/scripts/campaign/submarkets/SubmarketShared.java
+++ data/scripts/campaign/submarkets/SubmarketShared.java
@@ -4,13 +4,15 @@ public class SubmarketShared {
     static final boolean DEBUG = true; //false default
     static final float WEAPON_MULT = 2; // todo load from config
     static final float FIGHTER_MULT = 2; //todo load from config
-    static final float SHIP_MULT = 2; //todo load from config
-    static final float HULLMODS_MULT = 2; //todo include from config and add logic
-    static final boolean MANY_PICK = false; //false default
+    static final float SHIP_MULT = 2; //todo load from config //this not only means more ships but bigger, stronger ships too
+    static final float HULLMODS_MULT = 2; //todo include from config and default to 1
+    static final boolean MANY_PICK = true; //false default
     static final int WEAPON_PICKS = 2;
     static final int FIGHTER_PICKS = 2;
     static final int SHIP_PICKS = 2;
-    //static final int HULLMOD_PICKS = 2; //THIS IS NOT LOGICAL CAPTAIN
+    static final boolean EXTRA_TANKERS = true; //default to false
+
+    //todo check on load for anything over say 1000 and throw warning, hard cap to 10k? especially for picks lol
+
     //todo utility to load this from a function
-    //todo rounding logic function?
 }
